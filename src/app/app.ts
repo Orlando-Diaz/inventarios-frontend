92eed3c68b5f4f4ce2b10ec0d1a7679338d894cf
Orlando-Diaz <orlando.diazr@uqvirtual.edu.co>
2026-09-15 19:18:23 -0500

    feat: agregar navbar con Bootstrap y routerLink

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,8 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ProductoLista } from './producto-lista/producto-lista';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
-  imports: [ProductoLista],
+  imports: [RouterLink, RouterOutlet],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
