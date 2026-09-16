149170e5050c3d89b7a00008a5dbc5c7e80f4b23
Orlando-Diaz <orlando.diazr@uqvirtual.edu.co>
2026-09-15 19:18:41 -0500

    feat: configurar Angular Router con rutas inicial y agregar-producto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ProductoLista } from './producto-lista/producto-lista';
+import { ProductoForm } from './producto-form/producto-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ProductoLista },
+  { path: 'agregar-producto', component: ProductoForm },
+];
